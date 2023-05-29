b877d6c334ec46427ff9bcd9c3cd81fa6ed06a4a
AK Azad Milon <akazadmilon.dev@gmail.com>
2023-05-29 18:17:16 +0600

    Category and sub-category models

---
+++ models/categoryModels.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+
+const categorySchema = new Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    description:{
+        type: String,
+    },
+    isActive:{
+        type: Boolean,
+        default: false
+    },
+    status:{
+        type: String,
+        default: "waiting",
+        enum:["waiting", "approve", "rejected"]
+    },
+    subCategory:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "subCategory"
+        }
+    ],
+    update:{
+        type: Date,
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model("Category", categorySchema);
---
+++ models/subCategoryModels.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+
+const subCategorySchema = new Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    description:{
+        type: String,
+    },
+    isActive:{
+        type: Boolean,
+        default: false
+    },
+    status:{
+        type: String,
+        default: "waiting",
+        enum:["waiting", "approve", "rejected"]
+    },
+    category:{
+        type: Schema.Types.ObjectId,
+        ref:"Category"
+    },
+    update:{
+        type: Date,
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model("subCategory", subCategorySchema);
